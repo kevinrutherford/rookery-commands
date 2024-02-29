df2e9c1fe5b90a539d51e899dbfd2d41128a39ae
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-29 10:00:58 +0000

    Create a stream for new collections #12

--- src/write-resources/collection/create.ts
+++ src/write-resources/collection/create.ts
@@ -1,9 +1,18 @@
+import { EventStoreDBClient, jsonEvent, JSONEventType, NO_STREAM } from '@eventstore/db-client'
+import * as T from 'fp-ts/Task'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
 import { NonEmptyString } from 'io-ts-types/NonEmptyString'
 import { Command } from '../command'
 import { validateInput } from '../validate-input'
+
+type CollectionCreatedEvent = {
+  id: string,
+  handle: string,
+  name: string,
+  description: string,
+}
 
 const paramsCodec = t.type({
   id: NonEmptyString,
@@ -12,12 +21,34 @@ const paramsCodec = t.type({
   description: NonEmptyString,
 })
 
+type Params = t.TypeOf<typeof paramsCodec>
+
+type SomeEvent = JSONEventType<'collection-created', CollectionCreatedEvent>
+
+const send = (cmd: Params): TE.TaskEither<unknown, unknown> => {
+  const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
+
+  const event = jsonEvent<SomeEvent>({
+    type: 'collection-created',
+    data: {
+      ...cmd,
+      id: `/collections/${cmd.id}`,
+    },
+  })
+
+  return pipe(
+    T.of(client.appendToStream(event.data.id, event, { expectedRevision: NO_STREAM })),
+    TE.rightTask,
+  )
+}
+
 type Create = () => Command
 
 export const create: Create = () => (input) => pipe(
   input,
   validateInput(paramsCodec),
   TE.fromEither,
+  TE.map(send),
   TE.map(() => ({})),
 )
 
