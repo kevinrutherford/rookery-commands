1e9b1bb722bed28a483093a48ae31748ab82acae
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-20 18:59:07 +0000

    Add a command to update an entry #30

--- src/write-resources/entry/index.ts
+++ src/write-resources/entry/index.ts
@@ -1,2 +1,3 @@
 export { create } from './create'
+export { update } from './update'
 
--- src/write-resources/instantiate.ts
+++ src/write-resources/instantiate.ts
@@ -6,6 +6,7 @@ import { Action, Cmd } from '../http/index.open'
 export const instantiate = (): ReadonlyArray<Cmd> => [
   { path: '/collections', action: 'create' as Action, handler: collection.create() },
   { path: '/entries', action: 'create' as Action, handler: entry.create() },
+  { path: '/entries/:id', action: 'update' as Action, handler: entry.update() },
   { path: '/comments', action: 'create' as Action, handler: comment.create() },
 ]
 
