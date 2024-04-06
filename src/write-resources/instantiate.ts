b7c7ce9686559a5c89f315bad1a6b138e293d8b7
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-06 09:57:24 +0100

    Allow the work to be updated with Crossref status #43

--- src/write-resources/instantiate.ts
+++ src/write-resources/instantiate.ts
@@ -1,12 +1,13 @@
 import * as collection from './collection'
 import * as comment from './comment'
 import * as entry from './entry'
+import * as work from './work'
 import { Action, Cmd } from '../http/index.open'
 
 export const instantiate = (): ReadonlyArray<Cmd> => [
   { path: '/collections', action: 'create' as Action, handler: collection.create() },
   { path: '/entries', action: 'create' as Action, handler: entry.create() },
-  { path: '/entries/:id', action: 'update' as Action, handler: entry.update() },
+  { path: '/works/:id(10.*)', action: 'update' as Action, handler: work.update() },
   { path: '/comments', action: 'create' as Action, handler: comment.create() },
 ]
 
--- src/write-resources/work/update.ts
+++ src/write-resources/work/update.ts
@@ -1,4 +1,4 @@
-import { EventStoreDBClient, jsonEvent, JSONEventType, STREAM_EXISTS } from '@eventstore/db-client'
+import { EventStoreDBClient, jsonEvent, JSONEventType } from '@eventstore/db-client'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
@@ -9,38 +9,41 @@ import { validateInput } from '../validate-input'
 const paramsCodec = t.type({
   id: NonEmptyString,
   data: t.type({
-    type: t.literal('entry'),
+    type: t.literal('work'),
     id: NonEmptyString,
-    attributes: t.type({
-      frontMatter: t.type({
+    attributes: t.union([
+      t.type({ crossrefStatus: t.literal('not-determined') }),
+      t.type({ crossrefStatus: t.literal('not-found') }),
+      t.type({
+        crossrefStatus: t.literal('found'),
         title: NonEmptyString,
         abstract: NonEmptyString,
         authors: t.array(NonEmptyString),
       }),
-    }),
+    ]),
   }),
 })
 
 type Params = t.TypeOf<typeof paramsCodec>
 
-type FrontMatterAddedEventData = Params['data']['attributes'] & { entryId: string }
+type FrontMatterAddedEventData = Params['data']['attributes'] & { workId: string }
 
-type SomeEvent = JSONEventType<'front-matter-added', FrontMatterAddedEventData>
+type SomeEvent = JSONEventType<'work-updated', FrontMatterAddedEventData>
 
 const send = (cmd: Params): TE.TaskEither<ErrorOutcome, unknown> => {
   const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
 
   const event = jsonEvent<SomeEvent>({
-    type: 'front-matter-added',
+    type: 'work-updated',
     data: {
-      entryId: cmd.id,
+      workId: cmd.id,
       ...cmd.data.attributes,
     },
   })
 
   return pipe(
     TE.tryCatch(
-      async () => client.appendToStream(`entry.${cmd.data.id}`, event, { expectedRevision: STREAM_EXISTS }),
+      async () => client.appendToStream(`work.${cmd.data.id}`, event),
       (e) => [{
         title: 'Error appending to event stream',
         detail: JSON.stringify(e),
