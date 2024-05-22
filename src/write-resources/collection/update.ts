ef3440f51aa8510a66b50cbd168fa0c8971b5a70
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-22 17:52:16 +0100

    Push a dependency up the call stack #81

--- src/write-resources/collection/update.ts
+++ src/write-resources/collection/update.ts
@@ -2,7 +2,7 @@ import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
 import { NonEmptyString } from 'io-ts-types/NonEmptyString'
-import { appendStream } from '../../eventstore/append-stream'
+import { Eventstore } from '../../eventstore/eventstore'
 import { Command } from '../../http/index.open'
 import { validateInput } from '../validate-input'
 
@@ -18,19 +18,22 @@ const paramsCodec = t.type({
 
 type Params = t.TypeOf<typeof paramsCodec>
 
-const send = (cmd: Params) => appendStream(`collection.${cmd.data.id}`)({
-  type: 'collection-updated',
-  data: {
-    collectionId: cmd.data.id,
-    attributes: cmd.data.attributes,
+const send = (eventstore: Eventstore) => (cmd: Params) => pipe(
+  {
+    type: 'collection-updated',
+    data: {
+      collectionId: cmd.data.id,
+      attributes: cmd.data.attributes,
+    },
   },
-})
+  eventstore.appendStream(`collection.${cmd.data.id}`),
+)
 
-export const update = (): Command => (input) => pipe(
+export const update = (eventstore: Eventstore): Command => (input) => pipe(
   input,
   validateInput(paramsCodec),
   TE.fromEither,
-  TE.chain(send),
+  TE.chain(send(eventstore)),
   TE.map(() => ({})),
 )
 
--- src/write-resources/instantiate.ts
+++ src/write-resources/instantiate.ts
@@ -3,14 +3,24 @@ import * as comment from './comment'
 import * as community from './community'
 import * as entry from './entry'
 import * as work from './work'
+import { appendStream } from '../eventstore/append-stream'
+import { createStream } from '../eventstore/create-stream'
+import { Eventstore } from '../eventstore/eventstore'
 import { Action, Cmd } from '../http/index.open'
 
-export const instantiate = (): ReadonlyArray<Cmd> => [
-  { path: '/community', action: 'create' as Action, handler: community.create() },
-  { path: '/collections', action: 'create' as Action, handler: collection.create() },
-  { path: '/collections/:id', action: 'update' as Action, handler: collection.update() },
-  { path: '/entries', action: 'create' as Action, handler: entry.create() },
-  { path: '/works/:id(10.*)', action: 'update' as Action, handler: work.update() },
-  { path: '/comments', action: 'create' as Action, handler: comment.create() },
-]
+export const instantiate = (): ReadonlyArray<Cmd> => {
+  const eventstore: Eventstore = {
+    appendStream,
+    createStream,
+  }
+
+  return [
+    { path: '/community', action: 'create' as Action, handler: community.create() },
+    { path: '/collections', action: 'create' as Action, handler: collection.create() },
+    { path: '/collections/:id', action: 'update' as Action, handler: collection.update(eventstore) },
+    { path: '/entries', action: 'create' as Action, handler: entry.create() },
+    { path: '/works/:id(10.*)', action: 'update' as Action, handler: work.update() },
+    { path: '/comments', action: 'create' as Action, handler: comment.create() },
+  ]
+}
 
