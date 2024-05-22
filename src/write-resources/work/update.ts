e4b67562a785cea51b9d03459e33024e594fd291
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-22 12:50:32 +0100

    Remove duplicated Eventstore adapter code #81

--- src/write-resources/work/update.ts
+++ src/write-resources/work/update.ts
@@ -1,9 +1,9 @@
-import { EventStoreDBClient, jsonEvent, JSONEventType } from '@eventstore/db-client'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
 import { NonEmptyString } from 'io-ts-types/NonEmptyString'
-import { Command, ErrorOutcome } from '../../http/index.open'
+import { appendStream } from '../../eventstore/append-stream'
+import { Command } from '../../http/index.open'
 import { validateInput } from '../validate-input'
 
 const paramsCodec = t.type({
@@ -28,34 +28,13 @@ const paramsCodec = t.type({
 
 type Params = t.TypeOf<typeof paramsCodec>
 
-type WorkUpdatedEventData = {
-  workId: string,
-  attributes: Params['data']['attributes'],
-}
-
-type SomeEvent = JSONEventType<'work-updated', WorkUpdatedEventData>
-
-const send = (cmd: Params) => {
-  const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
-
-  const event = jsonEvent<SomeEvent>({
-    type: 'work-updated',
-    data: {
-      workId: cmd.data.id,
-      attributes: cmd.data.attributes,
-    },
-  })
-
-  return pipe(
-    TE.tryCatch(
-      async () => client.appendToStream(`work.${cmd.data.id}`, event),
-      (e): ErrorOutcome => [{
-        title: 'Error appending to event stream',
-        detail: JSON.stringify(e),
-      }],
-    ),
-  )
-}
+const send = (cmd: Params) => appendStream(`work.${cmd.data.id}`)({
+  type: 'work-updated',
+  data: {
+    workId: cmd.data.id,
+    attributes: cmd.data.attributes,
+  },
+})
 
 type Update = () => Command
 
