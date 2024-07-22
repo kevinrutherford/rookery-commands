7a6c676b020dae93bdcdc55e162c22c0512f0546
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-22 19:17:03 +0100

    Require the theme to be set when creating the community

--- src/write-resources/community/create.ts
+++ src/write-resources/community/create.ts
@@ -1,16 +1,42 @@
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
-import { NonEmptyString } from 'io-ts-types/NonEmptyString'
+import * as tt from 'io-ts-types/NonEmptyString'
 import { CommandHandler } from '../../http/command'
 import { Eventstore } from '../eventstore'
 import { validateInput } from '../validate-input'
 
+const theme = t.union([
+  t.literal('slate'),
+  t.literal('gray'),
+  t.literal('zinc'),
+  t.literal('neutral'),
+  t.literal('stone'),
+  t.literal('red'),
+  t.literal('orange'),
+  t.literal('amber'),
+  t.literal('yellow'),
+  t.literal('lime'),
+  t.literal('green'),
+  t.literal('emerald'),
+  t.literal('teal'),
+  t.literal('cyan'),
+  t.literal('sky'),
+  t.literal('blue'),
+  t.literal('indigo'),
+  t.literal('violet'),
+  t.literal('purple'),
+  t.literal('fuschia'),
+  t.literal('pink'),
+  t.literal('rose'),
+])
+
 const paramsCodec = t.type({
-  id: NonEmptyString,
-  name: NonEmptyString,
-  affiliation: NonEmptyString,
-  overview: t.array(NonEmptyString),
+  id: tt.NonEmptyString,
+  name: tt.NonEmptyString,
+  affiliation: tt.NonEmptyString,
+  overview: t.array(tt.NonEmptyString),
+  theme,
 })
 
 type Params = t.TypeOf<typeof paramsCodec>
