ab0085e9e4500838b0e48e1443450b38984df1c5
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-24 17:31:45 +0100

    Set the HTTP status code according to errors from the command handler

--- src/http/execute-command.ts
+++ src/http/execute-command.ts
@@ -3,7 +3,21 @@ import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import { StatusCodes } from 'http-status-codes'
 import { Command } from './command'
+import { ErrorOutcome } from './error-outcome'
 import { Logger } from './logger'
+
+const errorToStatus = (errors: ErrorOutcome): number => {
+  switch (errors[0].code) {
+    case 'bad-input':
+      return StatusCodes.BAD_REQUEST
+    case 'forbidden':
+      return StatusCodes.FORBIDDEN
+    case 'conflict':
+      return StatusCodes.CONFLICT
+    default:
+      return StatusCodes.INTERNAL_SERVER_ERROR
+  }
+}
 
 export type RouteHandler = Middleware
 
@@ -20,7 +34,7 @@ export const executeCommand: ExecuteCommand = (logger) => (command) => async (co
           params: JSON.stringify(context.params),
           body: JSON.stringify(context.request.body),
         })
-        context.response.status = StatusCodes.BAD_REQUEST
+        context.response.status = errorToStatus(errors)
         context.response.type = 'json'
         context.response.body = { errors }
       },
