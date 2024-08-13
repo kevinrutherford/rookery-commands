b93e4f6b1829cc5cc3f37e7570dcf70abcc99afd
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-13 15:29:58 +0100

    Summarise an inbox activity as an event

--- src/write-resources/inbox/create.ts
+++ src/write-resources/inbox/create.ts
@@ -10,30 +10,42 @@ import { validateInput } from '../validate-input'
 const paramsCodec = t.type({
   '@context': t.array(t.literal('https://www.w3.org/ns/activitystreams')),
   type: t.literal('Create'),
+  actor: t.type({
+    id: t.string,
+  }),
+  published: t.string,
   object: t.type({
+    type: t.literal('Note'),
     content: NonEmptyString,
+  }),
+  target: t.type({
+    type: t.literal('discussion'),
+    id: t.string,
   }),
 })
 
 type Params = t.TypeOf<typeof paramsCodec>
 
-const send = (eventstore: Eventstore, userId: string) => (cmd: Params) => {
+const send = (eventstore: Eventstore) => (cmd: Params) => {
   const id = v4()
   const event = {
     type: 'inbox:comment-created',
     data: {
-      ...cmd,
-      actorId: userId,
+      id,
+      actorId: cmd.actor.id,
+      publishedAt: cmd.published,
+      entryId: cmd.target.id,
+      content: cmd.object.content,
     },
   }
   return eventstore.createStream(`inbox:comment.${id}`)(event)
 }
 
-export const create: CommandHandler = (eventstore) => (input, userId) => pipe(
+export const create: CommandHandler = (eventstore) => (input) => pipe(
   input,
   validateInput(paramsCodec),
   TE.fromEither,
-  TE.chain(send(eventstore, userId)),
+  TE.chain(send(eventstore)),
   TE.map(() => ({})),
 )
 
