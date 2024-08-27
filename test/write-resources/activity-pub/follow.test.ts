532ef734b66247bdd6c8e0da15447548658be217
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-27 17:16:52 +0100

    Capture a defect in a failing test

---
+++ test/write-resources/activity-pub/follow.test.ts
@@ -0,0 +1,27 @@
+import * as Follow from '../../../src/write-resources/activity-pub/follow'
+import { arbitraryWord } from '../../helpers'
+
+describe('toRookeryEvent', () => {
+  const inboxUrl = arbitraryWord()
+  const activity = {
+    type: 'Follow',
+    actor: {
+      type: 'Person',
+      id: arbitraryWord(),
+      inbox: inboxUrl,
+    },
+    object: {
+      type: 'Person',
+      id: arbitraryWord(),
+    },
+  } satisfies Follow.FollowActivity
+  const event = Follow.toRookeryEvent(activity, arbitraryWord())
+
+  it.failing('sets the event inbox to the following actor\'s inbox', () => {
+    expect(event.data).toHaveProperty('remoteActorInboxUrl')
+    expect(event.data).toStrictEqual(expect.objectContaining({
+      remoteActorInboxUrl: inboxUrl,
+    }))
+  })
+})
+
